b337e78b19b4ca29c121378acc049a61aff31197
yokonao <yokotukanao@me.com>
2021-07-02 18:45:16 +0900

    rescue処理を追加

--- hoge.rb
+++ hoge.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
 
 p File.dirname(__FILE__)
 p File.join(File.dirname(__FILE__))
 p File.expand_path(File.join(File.dirname(__FILE__)))
-# coloredというジェムで色をつけれる
+# colored
+
+%w[bundler rubocop].each do |gem|
+  Gem::Specification.find_by_name(gem)
+rescue Gem::MissingSpecError
+  puts "#{gem} is missing!"
+  puts %(Please run "gem install #{gem}"")
+end
