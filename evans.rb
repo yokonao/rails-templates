b240d1a4bea71339977e230f074c0485f8de86c4
yokonao <yokotukanao@me.com>
2021-07-02 18:27:35 +0900

    必要なgemがインストールされているかのチェック

--- evans.rb
+++ evans.rb
@@ -1,8 +1,17 @@
-generate(:scaffold, "booking name:text start:datetime end:datetime")
-rails_command("db:migrate")
+%w[bundler rubocop].each do |gem|
+  next if Gem::Specification.find_by_name(gem)
+
+  run "gem install #{gem}"
+  Gem.refresh
+  Gem.activate(component)
+end
+
+run_bundle
+generate(:scaffold, 'booking name:text start:datetime end:datetime')
+rails_command('db:migrate')
 
 after_bundle do
   git :init
-  git add: "."
-  git commit: %Q{ -m 'Initial commit' }
+  git add: '.'
+  git commit: %( -m 'Initial commit' )
 end
