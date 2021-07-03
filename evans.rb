3f1079c172de05c89ecb03f0c99524e328e9201a
yokonao <yokotukanao@me.com>
2021-07-03 12:09:40 +0900

    マイグレーションをイニシャルコミットに含める

--- evans.rb
+++ evans.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # rails new booking-sample -d postgresql --webpack=react -m rails-templates/evans.rb
 
 def source_paths
@@ -16,11 +17,13 @@ end
 template 'docker-compose-postgresql.yml.tt', 'docker-compose.yml'
 template 'config/database.yml', 'config/database.yml', force: true
 run('mkdir tmp/pgdata')
+run('docker compose up -d')
 
 after_bundle do
   # https://github.com/rails/rails/issues/21700
   run 'spring stop'
   generate(:scaffold, 'booking name:text start:datetime end:datetime')
+  rails_command('db:migrate')
   git :init
   git add: '.'
   git commit: %( -m 'Initial commit' )
