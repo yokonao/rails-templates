ae5f61bf6b7fb2c763917518a05e8e1774283c02
yokonao <yokotukanao@me.com>
2021-08-02 12:04:50 +0900

    action mailerをスキップしない

--- create-rails-app.sh
+++ create-rails-app.sh
@@ -2,6 +2,6 @@
 
 app_name=$1
 rm -rf ../$app_name
-APP_NAME=$app_name rails new ../$app_name -d postgresql --skip-keeps --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-sprockets --skip-turbolinks --webpacker=react -m templates.rb 
+APP_NAME=$app_name rails new ../$app_name -d postgresql --skip-keeps --skip-action-mailbox --skip-action-text --skip-action-cable --skip-sprockets --skip-turbolinks --webpacker=react -m templates.rb 
 cd ../$app_name
 tmuxinator start .
